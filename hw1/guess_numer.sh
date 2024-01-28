e593b02c17e483f965a4a29eee117e5c24ff04d4
Panfilova Maria <88736099+MShpiz@users.noreply.github.com>
2024-01-28 17:17:43 +0300

    Update guess_numer.sh

--- hw1/guess_numer.sh
+++ hw1/guess_numer.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+check_number(){          # функция проверки равенства чисел
+if [ $2 -eq $1 ]; then    # еси числа равны выводит да иначе нет
+	echo "Yes"
+else
+	echo "No"
+fi;
+}
+
+a=10
+b=0
+echo "Guess my number"      
+
+while [ $b -ne $a ]        # пока числе не равны проверяем их равенство
+do
+	read b                  # считываем число
+	check_number $a $b      # проверяем число
+done
